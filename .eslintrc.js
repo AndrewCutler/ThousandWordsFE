2fa15b767406d15a27fc0ddc2791fcfaa00c7863
Andrew <andrewkcutler@gmail.com>
2022-11-26 14:42:40 -0500

    change dlint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,76 +5,93 @@ module.exports = {
   },
   extends: [
     'plugin:react/recommended',
-    'standard-with-typescript',
+    'plugin:@typescript-eslint/recommended',
     'airbnb',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:import/typescript',
     'prettier',
   ],
-  overrides: [
-    {
-      files: ['**/*.ts', '**/*.tsx'],
-      plugins: ['@typescript-eslint'],
-      extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-      parser: '@typescript-eslint/parser',
-      parserOptions: {
-        project: ['./tsconfig.json'],
-      },
-    },
-  ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
     ecmaFeatyres: { jsx: true },
-    project: ['./tsconfig.json'],
   },
   plugins: ['react', '@typescript-eslint', 'import', 'react-hooks'],
   rules: {
-    'no-return-await': [0],
-    'implicit-arrow-linebreak': [0],
-    'object-curly-newline': [0],
-    'max-len': [
-      'error',
-      {
-        code: 100,
-        tabWidth: 4,
-        ignoreUrls: true,
-        ignoreTemplateLiterals: true,
-      },
-    ],
-    '@typescript-eslint/strict-boolean-expressions': 'off',
-    'react/require-default-props': [0],
-    '@typescript-eslint/quotes': [
-      'error',
-      'single',
-      {
-        allowTemplateLiterals: true,
-      },
-    ],
-
-    '@typescript-eslint/no-misused-promises': [
-      'error',
-      {
-        checksVoidReturn: false,
-      },
-    ],
-    'react/no-unstable-nested-components': [
-      'warn',
-      {
-        allowAsProps: true,
-      },
-    ],
-    'jsx-quotes': ['warn', 'prefer-single'],
     quotes: ['warn', 'single'],
-    'import/extensions': ['warn', 'never'],
-    'react/jsx-filename-extension': [2, { extensions: ['.ts', '.tsx'] }],
-    'react/function-component-definition': [
+    'jsx-quotes': ['warn', 'prefer-single'],
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/jsx-filename-extension': [
       2,
       {
-        namedComponents: 'arrow-function',
-        unnamedComponents: 'arrow-function',
+        extensions: ['.ts', '.tsx'],
       },
     ],
+    'linebreak-style': ['warn', 'unix'],
+    'no-magic-numbers': [
+      2,
+      {
+        ignore: [-1, 0, 1, 2],
+      },
+    ],
+    'import/no-unresolved': [2, { caseSensitive: false }],
+    'react/function-component-definition': [0],
+    'no-tabs': [0],
+    indent: ['warn', 'tab', { SwitchCase: 1 }],
+    'react/jsx-indent': ['warn', 'tab'],
+    'react/jsx-indent-props': ['warn', 'tab'],
+    'react/jsx-props-no-spreading': 'off',
+    'react/jsx-wrap-multilines': 'off',
+    'import/extensions': ['warn', 'never'],
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'max-len': ['warn', 110],
+    'react/jsx-closing-bracket-location': ['warn', 'tag-aligned'],
+    'sort-imports': [
+      'off',
+      {
+        ignoreCase: false,
+        ignoreDeclarationSort: false,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['all', 'single', 'multiple', 'none'],
+        allowSeparatedGroups: true,
+      },
+    ],
+    'object-curly-newline': 'off',
+    'import/order': 'off',
+    'comma-dangle': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn'],
+    'implicit-arrow-linebreak': 0,
+    'no-confusing-arrow': 'off',
+    'react/require-default-props': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'operator-linebreak': 'off',
+    '@typescription-eslint/no-explicit-any': 'off',
+    'function-paren-newline': 'warn',
+    'no-unreachable': 'warn',
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+        // Choose from one of the "project" configs below or omit to use <root>/tsconfig.json by default
+        // use <root>/path/to/folder/tsconfig.json
+        project: 'path/to/folder',
+        // Multiple tsconfigs (Useful for monorepos)
+        // use a glob pattern
+        project: 'packages/*/tsconfig.json',
+        // use an array
+        project: [
+          'packages/module-a/tsconfig.json',
+          'packages/module-b/tsconfig.json',
+        ],
+        // use an array of glob patterns
+        project: ['packages/*/tsconfig.json', 'other-packages/*/tsconfig.json'],
+      },
+    },
   },
 };
--- api/queries.ts
+++ api/queries.ts
@@ -3,42 +3,82 @@ import { QueryFunction, QueryKey } from '@tanstack/react-query';
 import { Api, ApiConfig, HttpResponse, ImageDTO } from './api';
 
 interface IQuery {
-  queryKey: QueryKey;
-  queryFn: QueryFunction<any, QueryKey>;
+	queryKey: QueryKey;
+	queryFn: QueryFunction<
+		any,
+		QueryKey
+	>;
 }
 
 const apiConfig: ApiConfig = {
-  baseUrl: 'https://thousand-words.azurewebsites.net',
+	baseUrl: 'https://thousand-words.azurewebsites.net'
 };
 
 export const getImageById = (id: string): IQuery => {
-  const { api } = new Api(apiConfig);
+	const {
+		api
+	} =
+		new Api(
+			apiConfig
+		);
 
-  const queryKey = ['imageById', id];
-  const queryFn = async (): HttpResponse<ImageDTO, any> => {
-    const response = await api.imageList({ id });
+	const queryKey =
+		[
+			'imageById',
+			id
+		];
+	const queryFn =
+		async (): HttpResponse<
+			ImageDTO,
+			any
+		> => {
+			const response =
+				await api.imageList(
+					{
+						id
+					}
+				);
 
-    if (!response.ok) {
-      throw new Error('Failed to retrieve image by id.');
-    }
+			if (
+				!response.ok
+			) {
+				throw new Error(
+					'Failed to retrieve image by id.'
+				);
+			}
 
-    return response;
-  };
+			return response;
+		};
 
-  return { queryKey, queryFn };
+	return {
+		queryKey,
+		queryFn
+	};
 };
 
-export const createAlbum = async (
-  userId: string,
-  name: string
-): HttpResponse<void, any> => {
-  const { api } = new Api(apiConfig);
+export const createAlbum = async (userId: string, name: string): HttpResponse<void, any> => {
+	const {
+		api
+	} =
+		new Api(
+			apiConfig
+		);
 
-  const response = await api.albumCreate({ userId, name });
+	const response =
+		await api.albumCreate(
+			{
+				userId,
+				name
+			}
+		);
 
-  if (!response.ok) {
-    throw new Error('Failed to retrieve image by id.');
-  }
+	if (
+		!response.ok
+	) {
+		throw new Error(
+			'Failed to retrieve image by id.'
+		);
+	}
 
-  return response;
+	return response;
 };
